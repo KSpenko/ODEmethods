8f381a05d7964ed5d2eadb53114c0875ec77a3e1
KSpenko <kspenko.fmf@gmail.com>
2021-02-15 15:12:22 +0100

    Correcting syntax errors.

--- ODEmethods/rungekutta.py
+++ ODEmethods/rungekutta.py
@@ -181,8 +181,8 @@ class RKMethod:
     tolerance - parameter for the condition of the adaptive stepsize
     returns: (x, y, error) arrays of steps and their values and error estimate of values (if the method calculates it)."""
         x = numpy.arange(stepnum + 1, dtype=float)
-        y = numpy.empty([stepnum + 1, y0.shape[0]], dtype=float)
-        error = numpy.empty([stepnum + 1, y0.shape[0]], dtype=float)
+        y = numpy.empty([stepnum + 1, numpy.array(y0).shape[0]], dtype=float)
+        error = numpy.empty([stepnum + 1, numpy.array(y0).shape[0]], dtype=float)
 
         x = x * stepsize + x0
         y[0] = y0
