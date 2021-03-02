1646e81dfe3ee60476910049740582c91aeffec9
KSpenko <kspenko.fmf@gmail.com>
2021-03-02 21:53:39 +0100

    Added comparison with scipy ivp methods.

--- tests/newton_cooling/newton_cooling.py
+++ tests/newton_cooling/newton_cooling.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.integrate
 from ODEmethods.methods import rk_methods, predictor, corrector
 from ODEmethods.rungekutta import RKMethod 
 from ODEmethods.pece import PECE
@@ -20,6 +21,9 @@ rk1 = problem_rk.run(x0=0, y0=21, stepnum=100)
 problem_rk.method = rk_methods["cash_karp"]
 rk2 = problem_rk.run(x0=0, y0=21, stepnum=100, adaptive=True, tolerance=10e-9)
 
+problem_rk.method = rk_methods["dormand_prince"]
+rk3 = problem_rk.run(x0=0, y0=21, stepnum=100, adaptive=True, tolerance=10e-9)
+
 # PECE methods
 problem_pc = PECE(predictor[0], corrector[0], heat, par)
 pc1 = problem_pc.run(x0=0, y0=21, stepnum=100, runcorr=1)
@@ -28,6 +32,12 @@ problem_pc.predictor = predictor[3]
 problem_pc.corrector = corrector[2]
 pc2 = problem_pc.run(x0=0, y0=21, stepnum=100, runcorr=3)
 
+# Scipy methods
+sct = np.arange(0, 100, 1)
+sc1 = scipy.integrate.solve_ivp(heat, t_span=(0, 100), y0=[21], t_eval=sct)
+sc2 = scipy.integrate.odeint(heat, t=sct, y0=[21], tfirst=True)
+sc2 = np.transpose(sc2)[0]
+
 # Script for plotting graphs
 fig = plt.figure()
 plt.suptitle("Newton's law of cooling")
@@ -35,9 +45,12 @@ fig.set_size_inches(12, 7)
 
 plt.subplot(2, 2, 1)
 plt.errorbar(rk2[0], rk2[1], yerr=rk2[2], label='cash-karp', color='red')
-plt.plot(rk1[0], rk1[1], label='euler')
-plt.plot(pc1[0], pc1[1], label='p1c2_1')
-plt.plot(pc2[0], pc2[1], label='p4c4_3')
+plt.errorbar(rk3[0], rk3[1], yerr=rk3[2], label='dormand-prince', color='darkturquoise')
+plt.plot(rk1[0], rk1[1], label='euler', color='orange')
+plt.plot(pc1[0], pc1[1], label='p1c2_1', color='lime')
+plt.plot(pc2[0], pc2[1], label='p4c4_3', color='blue')
+plt.plot(sc1['t'], sc1['y'][0], label='scipy.solve_ivp', color='purple')
+plt.plot(sct, sc2, label='scipy.odeint', color='fuchsia')
 plt.plot(rk2[0], analytical_heat(rk2[0], 21, par), label='analytical', color='black', linestyle='--')
 plt.xlabel(r'$t$')
 plt.ylabel(r'$T(t)$')
@@ -45,9 +58,12 @@ plt.legend()
 
 plt.subplot(2, 2, 2)
 plt.errorbar(rk2[0], rk2[1], yerr=rk2[2], label='cash-karp', color='red')
-plt.plot(rk1[0], rk1[1], label='euler')
-plt.plot(pc1[0], pc1[1], label='p1c2_1')
-plt.plot(pc2[0], pc2[1], label='p4c4_3')
+plt.errorbar(rk3[0], rk3[1], yerr=rk3[2], label='dormand-prince', color='darkturquoise')
+plt.plot(rk1[0], rk1[1], label='euler', color='orange')
+plt.plot(pc1[0], pc1[1], label='p1c2_1', color='lime')
+plt.plot(pc2[0], pc2[1], label='p4c4_3', color='blue')
+plt.plot(sc1['t'], sc1['y'][0], label='scipy.solve_ivp', color='purple')
+plt.plot(sct, sc2, label='scipy.odeint', color='fuchsia')
 plt.plot(rk2[0], analytical_heat(rk2[0], 21, par), label='analytical', color='black', linestyle='--')
 plt.xlabel(r'$t$')
 plt.ylabel(r'$T(t)$')
@@ -55,24 +71,32 @@ plt.xlim(-1., 50.)
 plt.legend()
 
 plt.subplot(2, 2, 3)
-plt.errorbar(rk2[0], np.absolute(rk2[1]-analytical_heat(rk2[0], 21, par)), yerr=rk2[2], label='cash-karp', color='red')
-plt.plot(rk1[0], np.absolute(rk1[1]-analytical_heat(rk1[0], 21, par)), label='euler')
-plt.plot(pc1[0], np.absolute(pc1[1]-analytical_heat(pc1[0], 21, par)), label='p1c2_1')
-plt.plot(pc2[0], np.absolute(pc2[1]-analytical_heat(pc2[0], 21, par)), label='p4c4_3')
-plt.plot(rk2[0], rk2[2], label='ck_estimate', color='salmon', linestyle='--')
+plt.semilogy(rk2[0], rk2[2], label='ck_estimate', color='salmon', linestyle='--')
+plt.semilogy(rk3[0], rk3[2], label='dp_estimate', color='paleturquoise', linestyle='--')
+plt.semilogy(rk2[0], np.absolute(rk2[1]-analytical_heat(rk2[0], 21, par)), label='cash-karp', color='red')
+plt.semilogy(rk3[0], np.absolute(rk3[1]-analytical_heat(rk3[0], 21, par)), label='dormand-prince', color='darkturquoise')
+plt.semilogy(rk1[0], np.absolute(rk1[1]-analytical_heat(rk1[0], 21, par)), label='euler', color='orange')
+plt.semilogy(pc1[0], np.absolute(pc1[1]-analytical_heat(pc1[0], 21, par)), label='p1c2_1', color='lime')
+plt.semilogy(pc2[0], np.absolute(pc2[1]-analytical_heat(pc2[0], 21, par)), label='p4c4_3', color='blue')
+plt.semilogy(sc1['t'], np.absolute(sc1['y'][0]-analytical_heat(sc1['t'], 21, par)), label='scipy.solve_ivp', color='purple')
+plt.semilogy(sct, np.absolute(sc2-analytical_heat(sct, 21, par)), label='scipy.odeint', color='fuchsia')
 plt.xlabel(r'$t$')
-plt.ylabel(r'$\delta T(t)$')
+plt.ylabel(r'$\Delta T(t)$')
 plt.legend()
 
 plt.subplot(2, 2, 4)
-plt.errorbar(rk2[0], np.absolute(rk2[1]-analytical_heat(rk2[0], 21, par)), yerr=rk2[2], label='cash-karp', color='red')
-plt.plot(rk1[0], np.absolute(rk1[1]-analytical_heat(rk1[0], 21, par)), label='euler')
-plt.plot(pc1[0], np.absolute(pc1[1]-analytical_heat(pc1[0], 21, par)), label='p1c2_1')
-plt.plot(pc2[0], np.absolute(pc2[1]-analytical_heat(pc2[0], 21, par)), label='p4c4_3')
-plt.plot(rk2[0], rk2[2], label='ck_estimate', color='salmon', linestyle='--')
+plt.loglog(rk2[0], rk2[2], label='ck_estimate', color='salmon', linestyle='--')
+plt.loglog(rk3[0], rk3[2], label='dp_estimate', color='paleturquoise', linestyle='--')
+plt.loglog(rk2[0], np.absolute(rk2[1]-analytical_heat(rk2[0], 21, par)), label='cash-karp', color='red')
+plt.loglog(rk3[0], np.absolute(rk3[1]-analytical_heat(rk3[0], 21, par)), label='dormand-prince', color='darkturquoise')
+plt.loglog(rk1[0], np.absolute(rk1[1]-analytical_heat(rk1[0], 21, par)), label='euler', color='orange')
+plt.loglog(pc1[0], np.absolute(pc1[1]-analytical_heat(pc1[0], 21, par)), label='p1c2_1', color='lime')
+plt.loglog(pc2[0], np.absolute(pc2[1]-analytical_heat(pc2[0], 21, par)), label='p4c4_3', color='blue')
+plt.loglog(sc1['t'], np.absolute(sc1['y'][0]-analytical_heat(sc1['t'], 21, par)), label='scipy.solve_ivp', color='purple')
+plt.loglog(sct, np.absolute(sc2-analytical_heat(sct, 21, par)), label='scipy.odeint', color='fuchsia')
 plt.xlabel(r'$t$')
-plt.ylabel(r'$\delta T(t)$')
-plt.xlim(-1., 50.)
+plt.ylabel(r'$\Delta T(t)$')
+plt.xlim(-1., 10000.)
 plt.legend()
 
 plt.subplots_adjust(left=0.1, right=0.95, bottom=0.1, top=0.92, wspace=0.25, hspace=0.3)
