41f39360316f133448c03c7f9a82bc149dc6ca56
Kristof Spenko <kristof.spenko@cern.ch>
2021-09-21 15:37:28 +0200

    Small bugg.

--- examples/newton_cooling/newton_cooling.py
+++ examples/newton_cooling/newton_cooling.py
@@ -16,21 +16,21 @@ par=[0.1,-5.]
 
 # RK methods
 problem_rk = RKMethod(rk_methods["euler"], heat, par)
-rk1 = problem_rk.run(x0=0, y0=21, stepnum=100)
+rk1 = problem_rk.run(x0=0, y0=21, init_step=1.)
 
 problem_rk.method = rk_methods["cash_karp"]
-rk2 = problem_rk.run(x0=0, y0=21, stepnum=100, adaptive=True, tolerance=10e-9)
+rk2 = problem_rk.run(x0=0, y0=21, init_step=1., adaptive=True, tolerance=10e-9)
 
 problem_rk.method = rk_methods["dormand_prince"]
-rk3 = problem_rk.run(x0=0, y0=21, stepnum=100, adaptive=True, tolerance=10e-9)
+rk3 = problem_rk.run(x0=0, y0=21, init_step=1., adaptive=True, tolerance=10e-9)
 
 # PECE methods
 problem_pc = PECE(predictor[0], corrector[0], heat, par)
-pc1 = problem_pc.run(x0=0, y0=21, stepnum=100, runcorr=1)
+pc1 = problem_pc.run(x0=0, y0=21, stepnum=100, stepsize=1., runcorr=1)
 
 problem_pc.predictor = predictor[3]
 problem_pc.corrector = corrector[2]
-pc2 = problem_pc.run(x0=0, y0=21, stepnum=100, runcorr=3)
+pc2 = problem_pc.run(x0=0, y0=21, stepnum=100, stepsize=1., runcorr=3)
 
 # Scipy methods
 sct = np.arange(0, 100, 1)
@@ -44,11 +44,11 @@ plt.suptitle("Newton's law of cooling")
 fig.set_size_inches(12, 7)
 
 plt.subplot(2, 2, 1)
-plt.errorbar(rk2[0], rk2[1], yerr=rk2[2], label='cash-karp', color='red')
-plt.errorbar(rk3[0], rk3[1], yerr=rk3[2], label='dormand-prince', color='darkturquoise')
-plt.plot(rk1[0], rk1[1], label='euler', color='orange')
-plt.plot(pc1[0], pc1[1], label='p1c2_1', color='lime')
-plt.plot(pc2[0], pc2[1], label='p4c4_3', color='blue')
+plt.errorbar(rk2[0], rk2[:,1], yerr=rk2[:,2], label='cash-karp', color='red')
+plt.errorbar(rk3[0], rk3[:,1], yerr=rk3[:,2], label='dormand-prince', color='darkturquoise')
+plt.plot(rk1[0], rk1[:,1], label='euler', color='orange')
+plt.plot(pc1[0], pc1[:,1], label='p1c2_1', color='lime')
+plt.plot(pc2[0], pc2[:,1], label='p4c4_3', color='blue')
 plt.plot(sc1['t'], sc1['y'][0], label='scipy.solve_ivp', color='purple')
 plt.plot(sct, sc2, label='scipy.odeint', color='fuchsia')
 plt.plot(rk2[0], analytical_heat(rk2[0], 21, par), label='analytical', color='black', linestyle='--')
@@ -57,11 +57,11 @@ plt.ylabel(r'$T(t)$')
 plt.legend()
 
 plt.subplot(2, 2, 2)
-plt.errorbar(rk2[0], rk2[1], yerr=rk2[2], label='cash-karp', color='red')
-plt.errorbar(rk3[0], rk3[1], yerr=rk3[2], label='dormand-prince', color='darkturquoise')
-plt.plot(rk1[0], rk1[1], label='euler', color='orange')
-plt.plot(pc1[0], pc1[1], label='p1c2_1', color='lime')
-plt.plot(pc2[0], pc2[1], label='p4c4_3', color='blue')
+plt.errorbar(rk2[0], rk2[:,1], yerr=rk2[:,2], label='cash-karp', color='red')
+plt.errorbar(rk3[0], rk3[:,1], yerr=rk3[:,2], label='dormand-prince', color='darkturquoise')
+plt.plot(rk1[0], rk1[:,1], label='euler', color='orange')
+plt.plot(pc1[0], pc1[:,1], label='p1c2_1', color='lime')
+plt.plot(pc2[0], pc2[:,1], label='p4c4_3', color='blue')
 plt.plot(sc1['t'], sc1['y'][0], label='scipy.solve_ivp', color='purple')
 plt.plot(sct, sc2, label='scipy.odeint', color='fuchsia')
 plt.plot(rk2[0], analytical_heat(rk2[0], 21, par), label='analytical', color='black', linestyle='--')
@@ -71,10 +71,10 @@ plt.xlim(-1., 50.)
 plt.legend()
 
 plt.subplot(2, 2, 3)
-plt.semilogy(rk2[0], rk2[2], label='ck_estimate', color='salmon', linestyle='--')
-plt.semilogy(rk3[0], rk3[2], label='dp_estimate', color='paleturquoise', linestyle='--')
-plt.semilogy(rk2[0], np.absolute(rk2[1]-analytical_heat(rk2[0], 21, par)), label='cash-karp', color='red')
-plt.semilogy(rk3[0], np.absolute(rk3[1]-analytical_heat(rk3[0], 21, par)), label='dormand-prince', color='darkturquoise')
+plt.semilogy(rk2[0], rk2[:,2], label='ck_estimate', color='salmon', linestyle='--')
+plt.semilogy(rk3[0], rk3[:,2], label='dp_estimate', color='paleturquoise', linestyle='--')
+plt.semilogy(rk2[0], np.absolute(rk2[:,1]-analytical_heat(rk2[0], 21, par)), label='cash-karp', color='red')
+plt.semilogy(rk3[0], np.absolute(rk3[:,1]-analytical_heat(rk3[0], 21, par)), label='dormand-prince', color='darkturquoise')
 plt.semilogy(rk1[0], np.absolute(rk1[1]-analytical_heat(rk1[0], 21, par)), label='euler', color='orange')
 plt.semilogy(pc1[0], np.absolute(pc1[1]-analytical_heat(pc1[0], 21, par)), label='p1c2_1', color='lime')
 plt.semilogy(pc2[0], np.absolute(pc2[1]-analytical_heat(pc2[0], 21, par)), label='p4c4_3', color='blue')
@@ -85,10 +85,10 @@ plt.ylabel(r'$\Delta T(t)$')
 plt.legend()
 
 plt.subplot(2, 2, 4)
-plt.loglog(rk2[0], rk2[2], label='ck_estimate', color='salmon', linestyle='--')
-plt.loglog(rk3[0], rk3[2], label='dp_estimate', color='paleturquoise', linestyle='--')
-plt.loglog(rk2[0], np.absolute(rk2[1]-analytical_heat(rk2[0], 21, par)), label='cash-karp', color='red')
-plt.loglog(rk3[0], np.absolute(rk3[1]-analytical_heat(rk3[0], 21, par)), label='dormand-prince', color='darkturquoise')
+plt.loglog(rk2[0], rk2[:,2], label='ck_estimate', color='salmon', linestyle='--')
+plt.loglog(rk3[0], rk3[:,2], label='dp_estimate', color='paleturquoise', linestyle='--')
+plt.loglog(rk2[0], np.absolute(rk2[:,1]-analytical_heat(rk2[0], 21, par)), label='cash-karp', color='red')
+plt.loglog(rk3[0], np.absolute(rk3[:,1]-analytical_heat(rk3[0], 21, par)), label='dormand-prince', color='darkturquoise')
 plt.loglog(rk1[0], np.absolute(rk1[1]-analytical_heat(rk1[0], 21, par)), label='euler', color='orange')
 plt.loglog(pc1[0], np.absolute(pc1[1]-analytical_heat(pc1[0], 21, par)), label='p1c2_1', color='lime')
 plt.loglog(pc2[0], np.absolute(pc2[1]-analytical_heat(pc2[0], 21, par)), label='p4c4_3', color='blue')
